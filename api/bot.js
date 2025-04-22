3a684f560a7680e0f586d6d929012f2b9371d6be
Jhon-san29 <www.kaungkhantsan29@gmail.com>
2025-04-22 08:39:26 +0630

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -11,13 +11,21 @@ const CHANNELS = [
 
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    const body = req.body;
+    const msg = req.body.message;
+    if (!msg) return res.status(200).send('No message');
 
-    if (body.message && body.message.text) {
-      const msg = body.message;
-
-      for (const channel of CHANNELS) {
-        await bot.sendMessage(channel, msg.text);
+    for (const channel of CHANNELS) {
+      if (msg.photo) {
+        const fileId = msg.photo[msg.photo.length - 1].file_id;
+        const caption = msg.caption || '';
+        await bot.sendPhoto(channel, fileId, {
+          caption,
+          parse_mode: 'HTML'
+        });
+      } else if (msg.text) {
+        await bot.sendMessage(channel, msg.text, {
+          parse_mode: 'HTML'
+        });
       }
     }
 
